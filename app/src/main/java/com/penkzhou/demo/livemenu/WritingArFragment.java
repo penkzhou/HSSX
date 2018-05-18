b528f46eea3e09796f480885c00470e16e7d2a7a
田程 <tiancheng@meituan.com>
2018-05-18 20:23:40 +0800

    添加菜单按钮

--- app/src/main/java/com/penkzhou/demo/livemenu/WritingArFragment.java
+++ app/src/main/java/com/penkzhou/demo/livemenu/WritingArFragment.java
@@ -1,19 +1,29 @@
 package com.penkzhou.demo.livemenu;
 
-import android.Manifest;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.google.ar.sceneform.ux.ArFragment;
+public class WritingArFragment extends Fragment {
 
-public class WritingArFragment extends ArFragment {
     @Override
-    public String[] getAdditionalPermissions() {
-        String[] additionalPermissions = super.getAdditionalPermissions();
-        int permissionLength = additionalPermissions != null ? additionalPermissions.length : 0;
-        String[] permissions = new String[permissionLength + 1];
-        permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-        if (permissionLength > 0) {
-            System.arraycopy(additionalPermissions, 0, permissions, 1, additionalPermissions.length);
-        }
-        return permissions;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return new TextView(getContext());
     }
+
+//    @Override
+//    public String[] getAdditionalPermissions() {
+//        String[] additionalPermissions = super.getAdditionalPermissions();
+//        int permissionLength = additionalPermissions != null ? additionalPermissions.length : 0;
+//        String[] permissions = new String[permissionLength + 1];
+//        permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+//        if (permissionLength > 0) {
+//            System.arraycopy(additionalPermissions, 0, permissions, 1, additionalPermissions.length);
+//        }
+//        return permissions;
+//    }
 }
