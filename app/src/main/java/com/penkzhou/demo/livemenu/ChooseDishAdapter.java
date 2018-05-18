5c06b20ae76e92cbdc47a1f56423a147b98c1af1
zhoupeng05 <zhoupeng05@meituan.com>
2018-05-18 22:34:08 +0800

    add ui modify

---
+++ app/src/main/java/com/penkzhou/demo/livemenu/ChooseDishAdapter.java
@@ -0,0 +1,55 @@
+package com.penkzhou.demo.livemenu;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class ChooseDishAdapter extends RecyclerView.Adapter<ChooseDishAdapter.ChooseDishViewHolder> {
+
+    private List<DishModel> chooseList;
+
+    public ChooseDishAdapter(List<DishModel> chooseList) {
+        this.chooseList = chooseList;
+    }
+
+    @NonNull
+    @Override
+    public ChooseDishViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.choose_dish_item, parent, false);
+        return new ChooseDishViewHolder(textView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChooseDishViewHolder holder, int position) {
+        DishModel dishModel = chooseList.get(position);
+        if (dishModel != null) {
+            holder.bind(dishModel);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return chooseList == null ? 0 : chooseList.size();
+    }
+
+    static class ChooseDishViewHolder extends RecyclerView.ViewHolder {
+
+        public ChooseDishViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void bind(DishModel dishModel) {
+            if (itemView instanceof TextView) {
+                TextView textView = (TextView) itemView;
+                textView.setText(String.format("%s ¥%.0f x %s", dishModel.getName(), dishModel.getPrice(), dishModel.getChooseCount()));
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/penkzhou/demo/livemenu/Utils.java
+++ app/src/main/java/com/penkzhou/demo/livemenu/Utils.java
@@ -1,10 +1,49 @@
 package com.penkzhou.demo.livemenu;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
 
-public class Utils {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class Utils {
+
+    private Utils() {
+    }
+
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
+
+    public static String generateFilename() {
+        String date =
+                new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        return Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + File.separator + "Sceneform/" + date + "_screenshot.jpg";
+    }
+
+    public static void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+
+        File out = new File(filename);
+        if (!out.getParentFile().exists()) {
+            out.getParentFile().mkdirs();
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(filename);
+             ByteArrayOutputStream outputData = new ByteArrayOutputStream()) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData);
+            outputData.writeTo(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException ex) {
+            throw new IOException("Failed to save bitmap to disk", ex);
+        }
+    }
+
+
 }
